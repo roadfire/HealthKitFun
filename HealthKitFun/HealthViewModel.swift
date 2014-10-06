ec27ef79b392cabb88d1a07c268e3bc4128d6cc5
Josh Brown <josh@roadfiresoftware.com>
2014-10-06 16:56:20 -0400

    initial commit

---
+++ HealthKitFun/HealthViewModel.swift
@@ -0,0 +1,119 @@
+//
+//  HealthViewModel.swift
+//  HealthKitFun
+//
+//  Created by Josh Brown on 10/6/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthViewModel {
+    
+    var healthStore: HKHealthStore?
+    var steps = 0
+    var distance = 0.0
+    
+    init() {
+        self.healthStore = HKHealthStore()
+    }
+    
+    func requestAccessToDataTypes() {
+        let typesToRead = self.dataTypesToRead()
+        self.healthStore?.requestAuthorizationToShareTypes(NSSet(), readTypes:typesToRead, completion: { (success, error) -> Void in
+            if !success {
+                println("HealthKit can't access the data it needs to display these values.")
+                return
+            }
+            
+            self.fetchSteps(){}
+            self.fetchDistance(){}
+        })
+    }
+
+    func dataTypesToRead() -> NSSet {
+        let stepType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        let distanceType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        return NSSet(objects: stepType, distanceType)
+    }
+    
+    func fetchSteps(completion: () -> ()) {
+        let stepsType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        self.fetchMostRecentDataOfQuantityType(stepsType, anchor: 0) { (quantitySamples, newAnchor, error) -> () in
+            println("quantity samples: \(quantitySamples)")
+            
+            var steps = 0
+            if let samples = quantitySamples {
+                for sample in samples {
+                    steps += Int(sample.quantity.doubleValueForUnit(HKUnit.countUnit()))
+                    println("adding steps: \(steps)")
+                }
+            }
+            
+            self.steps = steps
+            completion()
+        }
+    }
+    
+    func fetchDistance(completion: () -> ()) {
+        let distanceType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        self.fetchTotalDataOfQuantityType(distanceType, completion: { (quantity, error) -> () in
+            self.distance = quantity.doubleValueForUnit(HKUnit.mileUnit())
+            completion()
+        })
+    }
+    
+    func fetchMostRecentDataOfQuantityType(quantityType: HKQuantityType, anchor: Int, completion:(quantitySamples: [HKQuantitySample]?, newAnchor: Int, error: NSError?) -> ()) {
+        let query = HKAnchoredObjectQuery(type: quantityType, predicate: nil, anchor: anchor, limit: 0) { (query, results, newAnchor, error) -> Void in
+            completion(quantitySamples: results as? [HKQuantitySample], newAnchor: newAnchor, error: error)
+        }
+        
+        self.healthStore?.executeQuery(query)
+    }
+    
+    func fetchTotalDataOfQuantityType(quantityType: HKQuantityType, completion:(quantity: HKQuantity, error: NSError?) -> ()) {
+        
+        let calendar = NSCalendar.currentCalendar()
+        let now = NSDate()
+        
+        let flags: NSCalendarUnit = .YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit
+        let components = calendar.components(flags, fromDate: now)
+
+        let startDate = calendar.dateFromComponents(components)
+        let endDate = calendar.dateByAddingUnit(NSCalendarUnit.DayCalendarUnit, value: 1, toDate: startDate!, options: nil)
+        
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: .StrictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: quantityType, quantitySamplePredicate: predicate, options: HKStatisticsOptions.CumulativeSum) { (query, result, error) -> Void in
+            completion(quantity: result.sumQuantity(), error: error)
+        }
+        self.healthStore?.executeQuery(query)
+    }
+    
+    func numberOfRowsInSection(section: Int) -> Int {
+        return 2
+    }
+    
+    func titleForRowAtIndexPath(indexPath: NSIndexPath) -> String {
+        switch indexPath.row {
+        case 0:
+            return "Steps"
+        case 1:
+            return "Walking/Running Distance"
+        default:
+            return "hmm...."
+        }
+    }
+    
+    func subtitleForRowAtIndexPath(indexPath: NSIndexPath) -> String {
+        switch indexPath.row {
+        case 0:
+            return "\(self.steps)"
+        case 1:
+            return "\(self.distance) mi"
+        default:
+            return "//TODO"
+        }
+    }
+}
--- HealthKitFun/ViewController.swift
+++ HealthKitFun/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import HealthKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let viewModel = HealthViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.addTarget(self, action: "refresh", forControlEvents: .ValueChanged)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func refresh() {
+        viewModel.fetchSteps { () -> () in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
+            }
+        }
+        viewModel.fetchDistance{ () -> () in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
+            }
+        }
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        if HKHealthStore.isHealthDataAvailable() {
+            viewModel.requestAccessToDataTypes()
+            self.refresh()
+        } else {
+            let alert = UIAlertController(title: "Health Data Unavailable", message: "Health Data is unavailable - this isn't going to work well...", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: { (action) in
+                // nothing to do
+            })
+            alert.addAction(action)
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }    
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        configureCell(cell, forRowAtIndexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.textLabel?.text = viewModel.titleForRowAtIndexPath(indexPath)
+        cell.detailTextLabel?.text = viewModel.subtitleForRowAtIndexPath(indexPath)
+    }
 
 }
 
